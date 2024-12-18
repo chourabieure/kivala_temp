1883a885e981f6b6bbd37ea27f26b4392b191bb5
Mattis SIMON <mattis@digitaledge.studio>
2024-12-18 12:18:29 +0100

    add links

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -6,6 +6,46 @@ import { SectionWrapper } from '../SectionWrapper/SectionWrapper'
 import { CONTACT_LINKS, OTHERS_LINKS, SOCIAL_NETWORKS_LINKS } from '@/constants/infos'
 import { NAVBAR_LINKS } from '../Navbar/Navbar.utils'
 
+const MEDIA_LINKS = {
+  mysweetimmo:
+    'https://www.mysweetimmo.com/2024/11/21/kivala-remporte-le-grand-prix-de-la-copro-2024-avec-son-systeme-innovant-dacces-aux-immeubles/',
+  'secret-defense':
+    'https://www.secret-defense.org/cybersecurite/3000-personnes-connaitront-le-code-dacces-a-votre-immeuble-dici-un-an/',
+  capital:
+    'https://www.capital.fr/immobilier/vous-seriez-surpris-de-savoir-combien-de-personnes-connaissent-le-code-de-votre-immeuble-1505788',
+  msn: 'https://www.msn.com/fr-fr/finance/autres/vous-seriez-surpris-de-savoir-combien-de-personnes-connaissent-le-code-de-votre-immeuble/ar-AA1uuK58',
+  presseagence:
+    'https://presseagence.fr/paris-dici-un-an-plus-de-3-000-personnes-connaitront-le-code-dacces-a-votre-immeuble/',
+  mysweetimmo2:
+    'https://www.mysweetimmo.com/2024/11/25/immobilier-dici-un-an-plus-de-3-000-personnes-connaitront-le-code-dacces-a-votre-immeuble/',
+  ledauphine:
+    'https://www.ledauphine.com/magazine-immobilier/2024/11/25/etes-vous-vraiment-en-securite-dans-votre-copropriete',
+  actu: 'https://actu.fr/ile-de-france/paris_75056/paris-insecurite-vols-plus-de-3000-personnes-connaitront-en-un-an-les-codes-d-acces-a-votre-immeuble_61918835.html',
+  bfmtv:
+    'https://www.bfmtv.com/paris/replay-emissions/bonjour-paris/ile-de-france-l-acces-a-vos-immeubles-pas-si-securise_VN-202411280261.html',
+  tf1info:
+    'https://www.tf1info.fr/immobilier/des-milliers-d-inconnus-ont-acces-au-digicode-de-votre-immeuble-comment-faire-changer-le-code-2335508.html',
+  'ouest-france':
+    'https://www.ouest-france.fr/ile-de-france/paris-75000/paris-pourquoi-le-digicode-serait-loin-de-proteger-efficacement-votre-immeuble-e5c6b27c-ad8f-11ef-ac7f-1983a1aa62e8',
+  'L’essentiel': 'https://infos-75.com/droits/infos-paris-83/',
+  cnews:
+    'https://www.cnews.fr/videos/france/2024-12-02/ca-va-retarder-lentree-mais-ca-nannule-rien-les-digicodes-loin-de-proteger',
+  mesinfos:
+    'https://mesinfos.fr/92100-boulogne-billancourt/plus-de-3-000-visiteurs-connaissent-le-code-d-acces-de-votre-immeuble-211488.html',
+  leprogres:
+    'https://www.leprogres.fr/magazine-immobilier/2024/12/02/copropriete-etes-vous-vraiment-en-securite-chez-vous',
+  dna: 'https://www.dna.fr/magazine-immobilier/2024/12/09/votre-copropriete-est-elle-vraiment-bien-securisee',
+  'republicain-lorrain':
+    'https://www.republicain-lorrain.fr/magazine-immobilier/2024/12/09/votre-copropriete-est-elle-vraiment-bien-securisee',
+  lejsl:
+    'https://www.lejsl.com/magazine-immobilier/2024/12/09/votre-copropriete-est-elle-vraiment-bien-securisee',
+  bienpublic:
+    'https://www.bienpublic.com/magazine-immobilier/2024/12/09/votre-copropriete-est-elle-vraiment-bien-securisee',
+  vosgesmatin:
+    'https://www.vosgesmatin.fr/magazine-immobilier/2024/12/09/votre-copropriete-est-elle-vraiment-bien-securisee',
+  'Sud Radio': 'https://youtu.be/bl2EjKv4Jd4?si=itbLxE9E3EDGoiA5]',
+}
+
 export const Footer = () => {
   return (
     <>
@@ -36,23 +76,24 @@ export const Footer = () => {
                 92100 Boulogne-Billancourt
               </span>
             </span>
-            <div className="flex flex-col gap-3">
-              <span className="text-base font-bold">SUIVEZ-NOUS</span>
-              <div className="flex gap-4">
-                {SOCIAL_NETWORKS_LINKS.map(({ name, href, icon }, index) => (
+            <span className="flex flex-col gap-3">
+              <span className="text-base font-bold">Presse / Médias</span>
+              <span className="grid grid-cols-2 gap-x-6 gap-y-3">
+                {Object.keys(MEDIA_LINKS).map((key, index) => (
                   <a
                     key={index}
-                    href={href}
-                    className="flex items-center rounded-full h-[48px] bg-background-kivala-secondary w-[48px]
-                      text-background-kivala-primary"
+                    href={MEDIA_LINKS[key as keyof typeof MEDIA_LINKS]}
+                    className="group flex items-center gap-4"
                   >
-                    <Image src={icon} alt={name} width={24} height={24} />
+                    <span className="text-base text-[#7B86A9] italic font-medium group-hover:underline">
+                      {key}
+                    </span>
                   </a>
                 ))}
-              </div>
-            </div>
+              </span>
+            </span>
           </div>
-          <span className="flex flex-col gap-3">
+          <span className="flex flex-col gap-3 min-w-[250px]">
             <span className="text-base font-bold">LIENS UTILES</span>
             <span className="flex flex-col gap-3">
               {NAVBAR_LINKS.map(({ label, href }, index) => (
@@ -119,6 +160,21 @@ export const Footer = () => {
                   </span>
                 </span>
               </span>
+            </div>
+            <div className="flex flex-col gap-3">
+              <span className="text-base font-bold">SUIVEZ-NOUS</span>
+              <div className="flex gap-4">
+                {SOCIAL_NETWORKS_LINKS.map(({ name, href, icon }, index) => (
+                  <a
+                    key={index}
+                    href={href}
+                    className="flex items-center rounded-full h-[48px] bg-background-kivala-secondary w-[48px]
+                      text-background-kivala-primary"
+                  >
+                    <Image src={icon} alt={name} width={24} height={24} />
+                  </a>
+                ))}
+              </div>
             </div>
           </div>
         </div>
